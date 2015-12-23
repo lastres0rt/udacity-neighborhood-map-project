765a7a7fe82801c73740c8c34660b5d9259be7e1
Rachel Keslensky <rach@lid.cc>
2015-12-22 22:49:23 -0500

    feat: tweak CSS and content

--- js/app.js
+++ js/app.js
@@ -1,46 +1,68 @@
-//  Source for the bindingHandler code:
+var initialLocations = [
+	{
+		name: 'Bracher Park',
+		lat: 37.370,
+		long: -122.002
+	},
+	{
+		name: 'Hacker Dojo',
+		lat: 37.402,
+		long: -122.052
+	}
 
-function initMap() {
+]
 
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 8,
-		center: {lat: -34.397, lng: 150.644}
-	});
+var map = {
+	init: function() {
+		this.map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 12,
+			center: {lat: 37.370, lng: -122.002}
+		});
 
-	mapElem = document.getElementById('map');
-	mapElem.style.height = window.innerHeight - 50;
+		var mapElem = document.getElementById('map');
+		mapElem.style.height = window.innerHeight - 50;
+	}
+}
 
-};
+var Location = function(data) {
+	this.name = ko.observable(data.name);
+	this.lat = ko.observable(data.lat);
+	this.long = ko.observable(data.long);
+
+	// Marker code grabbed from http://jsfiddle.net/t9wcC/
+	var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(data.lat, data.long),
+        title: data.name,
+        map: map,
+        draggable: true
+    });
+
+    google.maps.event.addListener(marker, 'drag', function() {
+        var pos = marker.getPosition();
+        this.lat(pos.lat());
+        this.long(pos.lng());
+    }.bind(this));
+
+    google.maps.event.addListener(marker, 'dragend', function() {
+        var pos = marker.getPosition();
+        this.lat(pos.lat());
+        this.long(pos.lng());
+    }.bind(this));
+}
 
 function AppViewModel() {
 	var self = this;
 
-	self.searchTerm = ko.observable("Search...");
+	this.searchTerm = ko.observable("Search...");
 
-	self.locations = ko.observableArray([
-			'Nashville',
-			'Norway',
-			'Bonair',
-			'Zimbabwe'
-		]);
+	this.locationList = ko.observableArray([]);
 
-	self.displayLocMarker = function(){
-		self.geocoder.geocode({'address': self.address()}, function(results, status) {
-			if (status === google.maps.GeocoderStatus.OK) {
-				map.setCenter(results[0].geometry.location);
-			 	self.marker = new google.maps.Marker({
-					map: map,
-					position: results[0].geometry.location
-				});
-			} else {
-				alert('Geocode was not successful for the following reason: ' + status);
-			};
-		});
-	};
-
+	initialLocations.forEach(function(locationItem){
+		self.locationList.push( new Location(locationItem) );
+	});
 }
 
 function startApp() {
-	initMap();
+	map.init();
 	ko.applyBindings(new AppViewModel());
 }
