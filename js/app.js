6ef00db7e7bb99c4197531a505f6e9695c7e87e4
Rachel Keslensky <rach@lid.cc>
2015-12-24 22:42:32 -0500

    feat: add Google Maps Markers

--- js/app.js
+++ js/app.js
@@ -12,42 +12,10 @@ var initialLocations = [
 
 ]
 
-var map = {
-	init: function() {
-		this.map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 12,
-			center: {lat: 37.370, lng: -122.002}
-		});
-
-		var mapElem = document.getElementById('map');
-		mapElem.style.height = window.innerHeight - 50;
-	}
-}
-
 var Location = function(data) {
 	this.name = ko.observable(data.name);
 	this.lat = ko.observable(data.lat);
 	this.long = ko.observable(data.long);
-
-	// Marker code grabbed from http://jsfiddle.net/t9wcC/
-	var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(data.lat, data.long),
-        title: data.name,
-        map: map,
-        draggable: true
-    });
-
-    google.maps.event.addListener(marker, 'drag', function() {
-        var pos = marker.getPosition();
-        this.lat(pos.lat());
-        this.long(pos.lng());
-    }.bind(this));
-
-    google.maps.event.addListener(marker, 'dragend', function() {
-        var pos = marker.getPosition();
-        this.lat(pos.lat());
-        this.long(pos.lng());
-    }.bind(this));
 }
 
 function AppViewModel() {
@@ -57,12 +25,26 @@ function AppViewModel() {
 
 	this.locationList = ko.observableArray([]);
 
+	this.map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 12,
+			center: {lat: 37.370, lng: -122.002}
+		});
+
+	this.mapElem = document.getElementById('map');
+	this.mapElem.style.height = window.innerHeight - 50;
+
 	initialLocations.forEach(function(locationItem){
 		self.locationList.push( new Location(locationItem) );
+		var latLng = {lat: locationItem.lat, lng: locationItem.long};
+		var marker = new google.maps.Marker({
+			position: latLng,
+			map: self.map,
+			title: locationItem.name
+		});
+
 	});
 }
 
 function startApp() {
-	map.init();
 	ko.applyBindings(new AppViewModel());
 }
